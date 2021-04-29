c25997a471d572bd850724421c4f127bfb6d752e
Kamil Kuzara <kamilkuzara@gmail.com>
2021-04-29 16:07:31 +0200

    Rename the default directory name for solutions in run-val.sh script

--- run-val.sh
+++ run-val.sh
@@ -2,7 +2,7 @@
 
 domain=$1
 problemNumber=$2
-version="concurrent-heuristics-BFS"
+version="concurrent_heuristics_BFS"
 runNumber=$3 # there may be more than one solution file for a problem so this is needed
 solutionPath="./solutions/$version/$domain-$problemNumber-$runNumber.sol"
 
