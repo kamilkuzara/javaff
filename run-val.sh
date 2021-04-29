9bef4e81f426b1e41ee44566f7c1186a3216f0cd
Kamil Kuzara <kamilkuzara@gmail.com>
2021-04-29 16:02:23 +0200

    Add more convenience scripts

---
+++ run-val.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+domain=$1
+problemNumber=$2
+version="parallel_EHC"
+runNumber=$3 # there may be more than one solution file for a problem so this is needed
+solutionPath="./solutions/$version/$domain-$problemNumber-$runNumber.sol"
+
+./run.sh $domain $problemNumber $solutionPath && ./validate.sh $domain $problemNumber $solutionPath
