daa4c939217d40610ebc28d9e123bd8f7d297296
Kamil Kuzara <kamilkuzara@gmail.com>
2021-08-05 00:33:29 +0200

    Update testing scripts

--- run-val.sh
+++ run-val.sh
@@ -2,8 +2,8 @@
 
 domain=$1
 problemNumber=$2
-version="parallel_BFS"
-runNumber=$3 # there may be more than one solution file for a problem so this is needed
-solutionPath="./solutions/$version/$domain-$problemNumber-$runNumber.sol"
+version="concurrent_BFS"
+threadsNo=$3
+solutionPath="./solutions/$version/$domain-$problemNumber-$(eval echo $threadsNo)_threads.sol"
 
 ./run.sh $domain $problemNumber $solutionPath && ./validate.sh $domain $problemNumber $solutionPath
